27ae6d498c982620509c578259e7534e4a5e6adf
Dmitry Semenov <lonelyteapot@gmail.com>
2021-10-17 22:45:31 +0300

    Import FieldModel.cpp into main.qml

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+#include "fieldmodel.h"
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
 
 int main(int argc, char *argv[])
@@ -10,7 +13,13 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    Field field {12, 8};
+    field.at(4, 4).state = Cell::TARGET;
+    FieldModel fieldModel {field};
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("_fieldModel", &fieldModel);
+
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
@@ -18,6 +27,7 @@ int main(int argc, char *argv[])
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
     engine.load(url);
+
 
     return app.exec();
 }
--- main.qml
+++ main.qml
@@ -1,9 +1,19 @@
 import QtQuick 2.15
 import QtQuick.Window 2.15
+import QtQuick.Layouts 1.0
 
 Window {
     width: 640
     height: 480
     visible: true
     title: qsTr("Visuals")
+
+    TableView {
+        anchors.fill: parent
+
+        model: _fieldModel
+        delegate: Text {
+            text: model.state
+        }
+    }
 }
